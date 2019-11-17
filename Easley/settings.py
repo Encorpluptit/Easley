1bab1c740de2a7591c2be773961031cf4bb9e119
BERNARD Damien <damien.bernard@epitech.eu>
2019-11-17 15:08:37 +0100

    [UPDATE] #PROJECT# Host in string.

--- Easley/settings.py
+++ Easley/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [easleymvp.herokuapp.com]
+ALLOWED_HOSTS = ['easleymvp.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [

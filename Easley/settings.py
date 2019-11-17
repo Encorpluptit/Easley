66f110e7dc3ca73cd3f302a5241a59441424ff0b
BERNARD Damien <damien.bernard@epitech.eu>
2019-11-17 15:03:45 +0100

    [UPDATE] #PROJECT# Adding secret key management.

--- Easley/settings.py
+++ Easley/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3blpq49h($g8x79c1djta$v=daj@jrhdy%hn47%!q$z#(1e_)c'
+# SECRET_KEY = '3blpq49h($g8x79c1djta$v=daj@jrhdy%hn47%!q$z#(1e_)c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

f5d45176233acc6d23524d272c1f42595ea4faa3
BERNARD Damien <damien.bernard@epitech.eu>
2019-12-12 23:20:20 +0100

    [FIX] #SETTINGS# Fixing email config.

--- Easley/settings.py
+++ Easley/settings.py
@@ -130,11 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = "de.meaux.bernard.damien@gmail.com"
-# EMAIL_HOST_PASSWORD = "PetitSuisseJoufflu4129"
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_PASSWORD = "kqtifbocgohviuor"
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Redirect AUTH_VIEWS
 LOGIN_URL = 'mvp-login'

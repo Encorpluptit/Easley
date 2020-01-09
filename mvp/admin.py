4884b91917387572a908ff37b3da9fbe1e8981bb
BERNARD Damien <damien.bernard@epitech.eu>
2020-01-09 13:27:43 +0100

    [FIX] #Admin# Fixing admin view.

--- mvp/admin.py
+++ mvp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Company, Commercial, Manager, Client, Conseil, License, Invoice, Service
+from .models import Company, Commercial, Manager, Client, Conseil, License, Invoice, Service, Invite
 
 # Register your models here.
 admin.site.register(Company)
@@ -11,3 +11,4 @@ admin.site.register(Conseil)
 admin.site.register(License)
 admin.site.register(Invoice)
 admin.site.register(Service)
+admin.site.register(Invite)
